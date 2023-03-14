a7f5948308fc471faa02b6b212a726e61b81d00c
Mikkel Wessel Nielsen <65943106+miwnKEA@users.noreply.github.com>
2023-03-14 18:13:04 +0100

    Update heart.py

--- heart.py
+++ heart.py
@@ -2,16 +2,16 @@ from sense_hat import SenseHat
 
 sense = SenseHat()
 
-r = (255, 0, 0)     # red
-o = (255, 128, 0)   # orange
-y = (255, 255, 0)   # yellow
-g = (0, 255, 0)     # green
-c = (0, 255, 255)   # cyan
-b = (0, 0, 255)     # blue
-p = (255, 0, 255)   # purple
-n = (255, 128, 128) # pink
-w = (255, 255, 255)  # white
-k = (0, 0, 0)       # blank
+r = (255, 0, 0)         # red
+o = (255, 128, 0)       # orange
+y = (255, 255, 0)       # yellow
+g = (0, 255, 0)         # green
+c = (0, 255, 255)       # cyan
+b = (0, 0, 255)         # blue
+p = (255, 0, 255)       # purple
+n = (255, 128, 128)     # pink
+w = (255, 255, 255)     # white
+k = (0, 0, 0)           # blank
 
 heart = [
     k, r, r, k, k, r, r, k,
